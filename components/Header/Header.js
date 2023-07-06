9ba84f44aab7fa8a6d4d9048e4675060a1ee885f
Xize Wu <wuxize1995@gmail.com>
2023-07-06 02:14:32 -0400

    add authcontext to login

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
 import Link from 'next/link';
+import { useAuth } from '../RightSideBar/AuthContext.';
 
 const Header = () => {
+  const { isLoggedIn, login, logout } = useAuth();
+  // setTimeout(()=>{login()}, 5000)
   return (
     <header className={styles.header}>
+          {console.log('props',isLoggedIn )}
       <h1>CloudCastle</h1>
       <div> <Link href='/register'>Register</Link>
       </div>
--- components/RightSideBar/AuthContext..js
+++ components/RightSideBar/AuthContext..js
@@ -1,12 +1,8 @@
-// AuthContext.js
 import { createContext, useContext, useState } from 'react';
 
-// Create the AuthContext
 const AuthContext = createContext();
 
-// Custom hook: useAuth
 export const useAuth = () => {
-  // Access the authentication state and functions using useContext
   const authContext = useContext(AuthContext);
   if (!authContext) {
     throw new Error('useAuth must be used within an AuthContextProvider');
@@ -14,14 +10,10 @@ export const useAuth = () => {
   return authContext;
 };
 
-// AuthContextProvider component
 export const AuthContextProvider = ({ children }) => {
-  // State for authentication
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // Functions for login and logout
   const login = () => {
-    // Perform login logic
     setIsLoggedIn(true);
   };
 
@@ -30,14 +22,12 @@ export const AuthContextProvider = ({ children }) => {
     setIsLoggedIn(false);
   };
 
-  // Value to be provided by the context
   const authContextValue = {
     isLoggedIn,
     login,
     logout,
   };
 
-  // Wrap the children components with the AuthContext.Provider
   return (
     <AuthContext.Provider value={authContextValue}>
       {children}
