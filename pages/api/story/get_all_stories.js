ffbc8f4bf619c749c5f3a6234715ae1f3c6d2ef2
Xize Wu <wuxize1995@gmail.com>
2023-07-17 17:42:40 -0400

    add link to individual story

--- pages/api/story/get_all_stories.js
+++ pages/api/story/get_all_stories.js
@@ -5,14 +5,22 @@ const prisma = new PrismaClient();
 export default async function get_all_stories(req, res) {
   if (req.method === 'GET') {
     try {
-      const rows = await prisma.stories.findMany();
-      res.status(200).json(rows)
+      const stories = await prisma.stories.findMany({
+        include: {
+          user: {
+            select: {
+              username: true
+            }
+          }
+        }
+      });
+
+      res.status(200).json(stories);
     } catch (error) {
-      console.error('Error fetching rows:', error);
+      console.error('Error fetching stories:', error);
       throw error;
     } finally {
       await prisma.$disconnect();
     }
-  
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import { Inter } from 'next/font/google';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 import StoryIntro from '../components/FrontPage/StoryIntro';
+import Link from 'next/link';
 // import styles from '@/styles/Home.module.css'
-const NoSSRHeader = dynamic(() => import('../components/Header/Header'), { ssr: false })
+const NoSSRHeader = dynamic(() => import('../components/Header/Header'), { ssr: false });
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export async function getStaticProps() {
   let stories = [];
 
   try {
-    // Make an HTTP request to your backend API to fetch all stories
     const response = await axios.get('http://localhost:3000/api/story/get_all_stories');
 
-    // Assuming the response contains an array of story objects
     stories = response.data;
-    console.log("stories", stories)
   } catch (error) {
     console.error('Error fetching data:', error);
   }
@@ -33,19 +31,21 @@ export async function getStaticProps() {
 
 export default function Home(props) {
   const username = Cookies.get('username');
-  const {stories} = props;
-  console.log(stories)
+  const { stories } = props;
+  console.log(stories);
 
   return (
     <>
-    <Head>
-      <title>CloudCastle</title>
-    </Head>
-      <NoSSRHeader username = {username}/>
-{/* <ul>
-  {stories.map(story=><li key={story.id}>{story.title}</li>)}
-</ul> */}
-{stories.map(story=><StoryIntro title = {story.title} username = {story.user_id} content = {story.content}/>)}
-  </>
-  )
+      <Head>
+        <title>CloudCastle</title>
+      </Head>
+      <NoSSRHeader username={username} />
+
+      {stories.map(story => (
+                <Link href={`/story/${story.id}`} key={story.id}>
+                  <StoryIntro title={story.title} username={story.user.username} content={story.content} />
+              </Link>      
+      ))}
+    </>
+  );
 }
